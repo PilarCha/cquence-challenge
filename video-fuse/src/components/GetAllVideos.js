d68eff41c7b824244cc3cef2b29eb5e6290eb941
PilarCha <pchavez92@gmail.com>
2021-09-22 11:47:18 -0700

    Created the fetch api call for getting all Videos. Stored it in an array

---
+++ video-fuse/src/components/GetAllVideos.js
@@ -0,0 +1,15 @@
+import * from React from 'react';
+import { DataGrid } from '@material-ui-grid'
+
+function GetAllVideos () {
+  const [data, setData] = React.useState(null);
+
+  // do this after render
+  React.useEffect(() => {
+    fetch("/video")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, [])
+}
+
+export default GettAllVideos;
