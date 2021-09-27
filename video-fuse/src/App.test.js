b7c8060ba9634480549aa76952c6ed59af725940
PilarCha <pchavez92@gmail.com>
2021-09-27 15:38:56 -0700

    Created a test to check if a video poster gets rendered correctly when a video example is passed down

--- video-fuse/src/App.test.js
+++ video-fuse/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import VideoPoster from './components/VideoPoster';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+it('renders correct video Poster', async () => {
+  const videoEx = {
+    "id": "25ff984c-e79d-460c-a75f-489e58425656",
+    "source": "https://video-fuse-footage.s3.us-east-1.amazonaws.com/astronaut-in-suit-closing-container-underwater.mp4",
+    "poster": "https://video-fuse-footage.s3.us-east-1.amazonaws.com/astronaut-in-suit-closing-container-underwater.jpg"
+  }
+  render(<VideoPoster key={1} video={videoEx}/>);
+  const linkElement = screen.getByRole('img');
   expect(linkElement).toBeInTheDocument();
 });
+
