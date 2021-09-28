271c9e2d277975ba8f30c58f23c0825cafdce244
PilarCha <pchavez92@gmail.com>
2021-09-27 17:59:14 -0700

    Of course the getByRole would return 1. We need to use getAllByRole

--- video-fuse/src/App.test.js
+++ video-fuse/src/App.test.js
@@ -6,15 +6,15 @@ import { ArrayContext, ArrayContextProvider } from './context/ArrayContext';
 const videoEx = {
   "id": "25ff984c-e79d-460c-a75f-489e58425656",
   "source": "https://video-fuse-footage.s3.us-east-1.amazonaws.com/astronaut-in-suit-closing-container-underwater.mp4",
-  "poster": "https://video-fuse-footage.s3.us-east-1.amazonaws.com/astronaut-in-suit-closing-container-underwater.jpg"
+  "poster": "https://video-fuse-footage.s3.us-east-1.amazounaws.com/astronaut-in-suit-closing-container-underwater.jpg"
 }
 
 describe("Video Poster", () => {
   
-  it('renders correct video Poster', async () => {   
+  it('renders a video Poster', async () => {   
     render(<VideoPoster key={1} video={videoEx}/>);
-    const linkElement = screen.getByRole('img');
-    expect(linkElement).toBeInTheDocument();
+    const linkElement = screen.getAllByRole('img');
+    expect(linkElement.length).toBe(1);
   });
 
   it('clicking on poster changes style', async () => {    
@@ -28,7 +28,7 @@ describe("Video Poster", () => {
       </ArrayContextProvider>
     );
     const linkElement = screen.getByRole('img');
-    fireEvent.click(linkElement)
+    fireEvent.click(linkElement);
     expect(linkElement).toHaveStyle('opacity:0.5');
   });
 }) 
