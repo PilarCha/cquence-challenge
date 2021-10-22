3fb71b37e2f739a356836440d550bc56549fa292
PilarCha <pchavez92@gmail.com>
2021-10-21 19:32:14 -0700

    eslint ranned

--- video-fuse/src/components/VideoPlayer.js
+++ video-fuse/src/components/VideoPlayer.js
@@ -1,36 +1,36 @@
-import ReactPlayer from 'react-player';
-import { useContext, useState } from 'react';
-import ArrayContext from '../context/ArrayContext.js'
+import ReactPlayer from "react-player";
+import { useContext, useState } from "react";
+import ArrayContext from "../context/ArrayContext.js";
 
 function VideoPlayer() {
-  const {array} = useContext(ArrayContext);
+  const { array } = useContext(ArrayContext);
   const [vidIdx, setVidIdx] = useState(0);
   const [autoPlay, setAutoPlay] = useState(false);
 
   const playNext = () => {
     const nextIdx = vidIdx + 1;
-    if(nextIdx >= array.length) {
+    if (nextIdx >= array.length) {
       setVidIdx(0);
       setAutoPlay(false);
       return;
     }
-    return setVidIdx(nextIdx)
-  }
+    return setVidIdx(nextIdx);
+  };
 
   const setToAutoPlay = () => {
     setAutoPlay(true);
-  }
+  };
 
-  return (        
+  return (
     <ReactPlayer
       data-testid="react-player-element"
-      url = {array[vidIdx]}
-      onStart= {setToAutoPlay}
-      onEnded= {playNext}
+      url={array[vidIdx]}
+      onStart={setToAutoPlay}
+      onEnded={playNext}
       playing={autoPlay}
       controls
     />
-  )
+  );
 }
 
 export default VideoPlayer;
