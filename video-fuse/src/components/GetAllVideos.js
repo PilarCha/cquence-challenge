20e025ce4101139bcb6ce0e80adfc6764e018ab4
PilarCha <pchavez92@gmail.com>
2021-10-21 19:31:59 -0700

    Running eslint on GetAllvideos

--- video-fuse/src/components/GetAllVideos.js
+++ video-fuse/src/components/GetAllVideos.js
@@ -1,40 +1,35 @@
-import * as React from 'react';
-import VideoPoster from './VideoPoster.js';
-import Grid from '@mui/material/Grid';
+import * as React from "react";
+import VideoPoster from "./VideoPoster.js";
+import Grid from "@mui/material/Grid";
 
-function GetAllVideos () {
+function GetAllVideos() {
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
     async function fetchVideoApi() {
-      let response = await fetch("http://localhost:3000/videos")
+      let response = await fetch("http://localhost:3000/videos");
       response = await response.json();
-      response = response.slice(0,8);
-      setData(response)
+      response = response.slice(0, 8);
+      setData(response);
     }
     fetchVideoApi();
   }, []);
 
   return (
-    <Grid sx={{flexGrow: 1}} container spacing={8}>
-
-        {data ? (
-          <Grid item xs={12}>
-            <Grid container justifyContent="center" spacing={2}>
-              {data.map(video => (
-                <VideoPoster 
-                  key={video.id} 
-                  video={video}
-                />
-              ))}
-            </Grid>
+    <Grid sx={{ flexGrow: 1 }} container spacing={8}>
+      {data ? (
+        <Grid item xs={12}>
+          <Grid container justifyContent="center" spacing={2}>
+            {data.map((video) => (
+              <VideoPoster key={video.id} video={video} />
+            ))}
           </Grid>
-        ) : (
-
-          <p> Loadiiiing </p>
-        )}
+        </Grid>
+      ) : (
+        <p> Loadiiiing </p>
+      )}
     </Grid>
-  )
+  );
 }
 
 export default GetAllVideos;
